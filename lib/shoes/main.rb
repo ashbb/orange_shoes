a0fac856ee0df7e2909d1052f7f85632d00af3d1
Zachary Scott <zachary.s.scott@gmail.com>
2011-01-20 00:53:25 -0500

    output file *.rb => *.html

--- lib/shoes/main.rb
+++ lib/shoes/main.rb
@@ -7,8 +7,10 @@ class Shoes
     app.instance_eval &blk
 
     repaint_all_by_order app
-
-    open 'orange_shoes_output.html', 'w' do |f|
+  
+    # $0 is the name of the input file
+    output = $0.gsub('.rb', '.html')
+    open output, 'w' do |f|
       f.puts HEADER % [app.width, app.height], 
         FUNCTIONS, VARS, INTERVALS, FOOTER
     end
